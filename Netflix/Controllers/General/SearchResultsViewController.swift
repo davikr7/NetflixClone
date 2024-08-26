0cdd63240d6bcba6b0344a148f1b706cb5bc16ab
davikr7 <david.krasnov7@gmail.com>
2024-08-26 03:51:34 +0300

    Creating SearchResultsViewController

---
+++ Netflix/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix
+//
+//  Created by David on 26.08.24.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let searchResultsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
+        
+        searchResultsCollectionView.dataSource = self
+        searchResultsCollectionView.delegate = self
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultsCollectionView.frame = view.bounds
+    }
+}
+
+extension SearchResultsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier,
+                                                            for: indexPath) as? TitleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.backgroundColor = .blue
+        return cell
+                
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+}
